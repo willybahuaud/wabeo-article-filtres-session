4f3539b09c73b44fd49d2863503978887933719b
Willy Bahuaud <rouge13scompany@hotmail.com>
2013-09-23 21:18:49 +0200

    Update filtre-meta.php

--- filtre-meta.php
+++ filtre-meta.php
@@ -1,33 +1,31 @@
 <?php
 
 //on ajoute ce champ de formulaire
-$genre = $_SESSION[ 'post-piscine' ];
-$terms = get_terms( 'piscine-bien' );
-if( ! is_wp_error( $terms ) {
+$piscine = $_SESSION[ 'post-piscine' ];
 ?>
     <p><label for="post-piscine">Piscine</label>
-    <input type="checkbox" value="1" id="post-piscine" name="post-piscine"></p>
+    <input type="hidden" value="0" name="post-piscine">
+    <input type="checkbox" value="1" id="post-piscine" name="post-piscine" <?php checked( true, $piscine ); ?>></p>
 <?php
-}
+
 
 //dans switch_session() on sauvegarde la valeur de post-genre,
 //on sinon on en défini une par défaut
-if( isset( $_POST[ 'post-genre' ] ) ) {
-    $terms = get_terms( 'genre-bien' );
-    $_SESSION[ 'post-genre' ] = ( in_array( $_POST[ 'post-genre' ], wp_list_pluck( $terms, 'slug' ) ) ) ? $_POST[ 'post-genre' ] : false;
+if( isset( $_POST[ 'post-piscine' ] ) ) {
+    $_SESSION[ 'post-piscine' ] = ( 1 == $_POST[ 'post-piscine' ] ) ? true : false;
 }
 
-if( ! isset( $_SESSION[ 'post-genre' ] ) )
-    $_SESSION[ 'post-genre' ] = false;
+if( ! isset( $_SESSION[ 'post-piscine' ] ) )
+    $_SESSION[ 'post-piscine' ] = false;
 
 //dans switch_output_order
 //dans la condition
-if( false !== $_SESSION[ 'post-genre' ] ) {
+if( false !== $_SESSION[ 'post-piscine' ] ) {
     $q->set( 'meta_query', array(
         'relation' => 'AND',
         array(
-            'key' => 'genre-bien',
-            'value'    => $_SESSION[ 'post-genre' ],
+            'key' => '_equipement',
+            'value'    => 'piscine',
             'compare' => '='
             )
         ) );
